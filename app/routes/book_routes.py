4646ea1678ac13f5bc77b2c4197c8814e40f2be7
Mikaela Baluyot <mikaebal@gmail.com>
2025-04-28 00:02:07 -0700

    refactored get_one_book and validate_book. added put and delete

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -43,7 +43,62 @@ def get_all_books():
     return books_response
 
 
+# lesson 4
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+# updating a book endpoint
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+# deleting endpoint separately
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response , 400))
+
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+    
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+
+    return book
+
+
+
+
+
 # lesson 2
+
+# get_all_books hard coded book record
 # @books_bp.get("")
 # def get_all_books():
 #     books_response = []
@@ -57,7 +112,7 @@ def get_all_books():
 #         )
 #     return books_response
 
-
+# hard coded book record
 # @books_bp.get("/<book_id>")
 # def get_one_book(book_id):
 #     book = validate_book(book_id)
