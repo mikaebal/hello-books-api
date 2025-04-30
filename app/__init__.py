e6eed2109de958fdde6af5615838db7223fe6961
Mikaela Baluyot <mikaebal@gmail.com>
2025-04-30 00:02:09 -0700

    lesson 6 refactor

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from .db import db, migrate
 from .models import book
 from .routes.book_routes import books_bp
+# lesson 6
+import os
 
 
 # lesson 1
@@ -12,7 +14,7 @@ from .routes.book_routes import books_bp
 from .routes.book_routes import books_bp
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
     # lesson 1 - Register Blueprints here
@@ -20,8 +22,17 @@ def create_app():
 
     # lesson 3
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     # connection string to our database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    
+    # Lesson 6 refactor - sets value from development database in .env
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
 
     # connects to Flask app
     db.init_app(app)
